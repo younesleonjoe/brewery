a5c90684c4d75b903223c031016784d84563bb8d
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-06 01:35:48 +0300

    Add validation to CustomerDTO vq

--- src/main/java/com/younesleonjoe/brewery/customer/v1/CustomerDTO.java
+++ src/main/java/com/younesleonjoe/brewery/customer/v1/CustomerDTO.java
@@ -1,17 +1,20 @@
 package com.younesleonjoe.brewery.customer.v1;
 
+import jakarta.validation.constraints.*;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CustomerDTO {
-	private UUID id;
-	private String name;
+  private UUID id;
+
+  @NotBlank
+  @Size(min = 3, max = 100)
+  private String name;
 }
