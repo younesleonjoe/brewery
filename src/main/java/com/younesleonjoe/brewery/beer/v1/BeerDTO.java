e34c949b596e61023cbeb02730637ae5a6daa9fb
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-06 01:36:56 +0300

    Add validation for BeerDTO v1

--- src/main/java/com/younesleonjoe/brewery/beer/v1/BeerDTO.java
+++ src/main/java/com/younesleonjoe/brewery/beer/v1/BeerDTO.java
@@ -1,19 +1,23 @@
 package com.younesleonjoe.brewery.beer.v1;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import java.util.UUID;
+
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDTO {
-  private UUID id;
-  private String name;
-  private String style;
-  private Long upc;
+
+  @Null private UUID id;
+  @NotBlank private String name;
+  @NotBlank private String style;
+  @Positive private Long upc;
 }
