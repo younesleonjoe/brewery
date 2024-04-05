e6ebd7f9953db8ad7eb0b1285d8707a8a0caaee8
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-05 15:33:03 +0300

    Add unit tests for BreweryClient class

---
+++ src/test/java/com/younesleonjoe/brewery/client/BreweryClientTest.java
@@ -0,0 +1,68 @@
+package com.younesleonjoe.brewery.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.younesleonjoe.brewery.beer.v1.BeerDTO;
+import com.younesleonjoe.brewery.customer.v1.CustomerDTO;
+import java.net.URI;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryClientTest {
+
+  @Autowired
+  BreweryClient breweryClient;
+
+  @Test
+  void testGetBeerById() {
+    BeerDTO beerDTO = breweryClient.getBeerById(UUID.randomUUID());
+    assertNotNull(beerDTO);
+  }
+
+  @Test
+  void testCreateBeer() {
+    BeerDTO beerDTO = BeerDTO.builder().id(UUID.randomUUID()).build();
+    URI uri = breweryClient.createBeer(beerDTO);
+    assertNotNull(uri);
+    System.out.println(uri);
+  }
+
+  @Test
+  void testUpdateBeer() {
+    BeerDTO beerDTO = BeerDTO.builder().id(UUID.randomUUID()).build();
+    breweryClient.updateBeer(UUID.randomUUID(), beerDTO);
+  }
+
+  @Test
+  void testDeleteBeer() {
+    breweryClient.deleteBeer(UUID.randomUUID());
+  }
+
+  @Test
+  void testGetCustomerById() {
+    CustomerDTO customerDTO = breweryClient.getCustomerById(UUID.randomUUID());
+    assertNotNull(customerDTO);
+  }
+
+  @Test
+  void testCreateCustomer() {
+    CustomerDTO customerDTO = CustomerDTO.builder().id(UUID.randomUUID()).build();
+    URI uri = breweryClient.createCustomer(customerDTO);
+    System.out.println(uri);
+    assertNotNull(uri);
+  }
+
+  @Test
+  void testUpdateCustomer() {
+    CustomerDTO customerDTO = CustomerDTO.builder().id(UUID.randomUUID()).build();
+    breweryClient.updateCustomer(UUID.randomUUID(), customerDTO);
+  }
+
+  @Test
+  void testDeleteCustomer() {
+    breweryClient.deleteCustomer(UUID.randomUUID());
+  }
+}
