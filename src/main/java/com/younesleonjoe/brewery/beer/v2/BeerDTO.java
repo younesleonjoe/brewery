99a5902d39d652c823ef8afc6d691f0c0709f6db
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-06 01:36:05 +0300

    Add validation to CustomerDTO v2

--- src/main/java/com/younesleonjoe/brewery/beer/v2/BeerDTO.java
+++ src/main/java/com/younesleonjoe/brewery/beer/v2/BeerDTO.java
@@ -1,5 +1,11 @@
 package com.younesleonjoe.brewery.beer.v2;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,8 +17,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class BeerDTO {
-	private UUID id;
-	private String name;
-	private BeerStyleEnum style;
-	private Long upc;
+  @Null private UUID id;
+  @NotBlank private String name;
+  @NotNull private BeerStyleEnum style;
+  @Positive @NotNull private Long upc;
+
+  private Long version;
+
+  @Null private OffsetDateTime createdAt;
+  @Null private OffsetDateTime updatedAt;
+
+  @Positive @NotNull private BigDecimal price;
+
+  private Integer quantityOnHand;
 }
